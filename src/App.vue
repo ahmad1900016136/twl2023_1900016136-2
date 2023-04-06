2d7fb43e19701caa98ff7f8efe0cdb9bd7f44607
ahmad1900016136 <ahmad1900016136@webmail.uad.ac.id>
2023-04-06 22:21:59 +0700

    project new

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+  <navbar-component />
+  <router-view />
+ </div> 
+</template>
+
+<script>
+import NavbarComponent from './components/NavbarComponent.vue'
+export default {
+  //register the component
+  components :{NavbarComponent}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav-container">
+    <router-link to="/">home</router-link>
+    <router-link to="/about">about</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav-container {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+}
+</style>
